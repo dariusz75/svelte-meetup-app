f255464297787ad2439e7575d660ebf2794c5cc5
Dariusz Franczak <dariuszfranczak@dariuszs-mbp.home>
2021-07-20 17:37:47 +0200

    meetups rendered and restyled

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,33 @@
 <script>
 import Header from './UI/Header.svelte'
+import MeetupGrid from './Meetups/MeetupGrid.svelte'
+
+const meetups = [
+  {
+    id: 'm1',
+    title: 'HTML Basics',
+    subtitle: 'Learn HTML in two hours',
+    description: 'In this meetup we will have an expert who will teach you HTML fundamentals.',
+    imageUrl: 'https://www.tutorialrepublic.com/lib/images/html-illustration.png',
+    address: '123 Brixton Hill, London',
+    email: 'codingmeatup@google.com'
+  },
+  {
+    id: 'm2',
+    title: 'JS Basics',
+    subtitle: 'Learn to code in two hours',
+    description: 'In this meetup we will have an expert who will teach you Javascript fundamentals.',
+    imageUrl: 'https://www.pozycjonusz.pl/wp-content/uploads/2019/05/java-script-SEO.jpg',
+    address: '123 Brixton Hill, London',
+    email: 'codingmeatup@google.com'
+  }
+]
 </script>
 
 <Header />
+<MeetupGrid meetups={meetups} />
 
+
+<style>
+  
+</style>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,33 @@
+<script>
+  import MeetupItem from './MeetupItem.svelte'
+
+  export let meetups
+</script>
+
+<section class="meetups">
+  {#each meetups as meetup }
+    <MeetupItem 
+      title={meetup.title} 
+      subtitle={meetup.subtitle} 
+      imageUrl={meetup.imageUrl} 
+      description={meetup.description} 
+      email={meetup.email} 
+      address={meetup.address}/>
+  {/each}
+</section>
+
+<style>
+  section {
+    margin-top: 5rem;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media(min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
